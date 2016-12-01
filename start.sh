d2910c01fec9745eacb0829ec14bf4ca688397f0
brelevenix <eric.debeau@gmail.com>
2016-12-01 22:22:39 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get install r-base -y
 sudo su - \
 -c "R -e \"packages <- c('shiny','rmarkdown','infotheo', 'minerva', 'visNetwork', 'rAmCharts','mclust','Hmisc'); if (length(setdiff(packages, rownames(installed.packages()))) > 0){install.packages(setdiff(packages, rownames(installed.packages())), repos='https://cran.rstudio.com/')}\""
-apt-get install gdebi-core -y
+apt-get install gdebi-core
 wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.5.1.834-amd64.deb
 sudo gdebi shiny-server-1.5.1.834-amd64.deb
 sudo systemctl start shiny-server
